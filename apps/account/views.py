2739701c5fe86cf56d270c17d7c61dd718185cb9
Sergio <sergio.pm.developer@gmail.com>
2024-12-26 18:29:41 +0000

    feat: update account details and display a success toast in case the transaction was successful

--- apps/account/views.py
+++ apps/account/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 
+from apps.account.forms import AccountDetailsForm
+
 
 class AccountDetailsView(View):
     """
@@ -28,3 +30,32 @@ class AccountDetailsView(View):
         """
 
         return render(request, "account/account-details.html")
+
+    def post(self, request: WSGIRequest) -> HttpResponse:
+        """
+        Handles the account details form submission.
+
+        Parameters
+        ----------
+        request : WSGIRequest
+            The request object.
+
+        Returns
+        -------
+        HttpResponse
+            The account details page with the form if the form is invalid,
+            otherwise the account details page with the result as success.
+        """
+
+        account_details_form = AccountDetailsForm(request.POST, instance=request.user)
+
+        if not account_details_form.is_valid():
+            return render(
+                request,
+                "authentication/sign-in.html",
+                {"form": account_details_form},
+            )
+
+        account_details_form.save()
+
+        return render(request, "account/account-details.html", {"result": "success"})
