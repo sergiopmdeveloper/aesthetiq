bd649d5b4537ddcb4bb726d4aa56f8925b11967b
Sergio <sergio.pm.developer@gmail.com>
2024-12-29 18:26:05 +0000

    feat: implement initial background removal logic of an image

--- apps/tools/views.py
+++ apps/tools/views.py
@@ -1,9 +1,13 @@
+import io
+
 from django.contrib.auth.decorators import login_required
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
+from PIL import Image
+from rembg import remove
 
 
 class RemoveBackgroundView(View):
@@ -29,14 +33,14 @@ class RemoveBackgroundView(View):
 
         return render(request, "tools/remove-background.html")
 
-    def post(self, _: WSGIRequest) -> HttpResponse:
+    def post(self, request: WSGIRequest) -> HttpResponse:
         """
         Handles the remove background request.
 
         Parameters
         ----------
-        _ : WSGIRequest
-            The request object, not used for now.
+        request : WSGIRequest
+            The request object.
 
         Returns
         -------
@@ -44,4 +48,25 @@ class RemoveBackgroundView(View):
             ...
         """
 
-        return HttpResponse("Not implemented yet...")
+        input_image_file = request.FILES.get("image")
+
+        if not input_image_file:
+            return render(request, "tools/remove-background.html", {"error": "No image provided."})
+
+        try:
+            input_img = Image.open(input_image_file)
+
+            input_img_buffer = io.BytesIO()
+            input_img.save(input_img_buffer, format=input_img.format)
+            input_img_bytes = input_img_buffer.getvalue()
+
+            output_img_bytes = remove(input_img_bytes)
+            output_img_format = input_img.format.lower()
+            output_img_name = input_image_file.name + "_no_bg" + output_img_format
+
+            response = HttpResponse(output_img_bytes, content_type=f"image/{output_img_format}")
+            response["Content-Disposition"] = f'attachment; filename="{output_img_name}"'
+
+            return response
+        except Exception:
+            return render(request, "tools/remove-background.html", {"error": "An error ocurred."})
